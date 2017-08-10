7e90aec048117a246a3a42cfb70d4104d15d4fd3
meghal <meghalbulsara22@gmail.com>
2017-08-10 13:00:45 +0530

    Essential library added

---
+++ digest.rb
@@ -0,0 +1,11 @@
+require "digest"
+
+Digest::SHA1.digest         "my-password"
+Digest::SHA1.hexdigest      "my-password"
+Digest::SHA1.base64digest   "my-password"
+Digest::MD5.digest          "my-password"
+Digest::MD5.hexdigest       "my-password"
+Digest::MD5.base64digest    "my-password"
+Digest::SHA256.digest       "my-password"
+Digest::SHA256.hexdigest    "my-password"
+Digest::SHA256.base64digest "my-password"
